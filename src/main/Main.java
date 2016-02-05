a4111133adc8c85a1cefcc83278823ad74d3eedd
adriansb3105 <adriansb3105@gmail.com>
2016-02-05 12:13:14 -0600

    Proyecto en blanco, a partir de aqui se empieza a trabajar

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author adriansb3105
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
